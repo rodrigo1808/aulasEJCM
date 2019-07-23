7a155fad9f95fabda5090a56f5ab39ba3467a5d0
Rodrigo <rodrigobragavasco@gmail.com>
2019-07-23 01:24:23 -0300

    + lógica da pagina pokemons, até a criação do componente

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { PokemonsPage } from './pokemons.page';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 const routes: Routes = [
   {
@@ -19,8 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
   ],
-  declarations: [PokemonsPage]
+  declarations: [PokemonsPage, PokemonCardComponent]
 })
 export class PokemonsPageModule {}
--- src/app/pokemons/pokemons.page.ts
+++ src/app/pokemons/pokemons.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../service/pokemon.service';
 
 @Component({
   selector: 'app-pokemons',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonsPage implements OnInit {
 
-  constructor() { }
+  time = 2;
+  pokemons = [];
+
+  constructor(private pokemonService: PokemonService) { }
 
   ngOnInit() {
   }
+
+  waitCounter() {
+    const interval = setInterval(() => {
+
+      this.time--;
+
+      if (this.time === 0) {
+        this.time = 2;
+        clearInterval(interval);
+      }
+
+    }, 1000);
+  }
+
+  getRandomPokemonID() {
+    return Math.floor(Math.random() * 807) + 1;
+  }
+
+  catchPokemon() {
+    this.waitCounter();
+    this.pokemonService.getPokemon(this.getRandomPokemonID()).subscribe(
+      (res) => {
+        console.log(res);
+        this.pokemons.push(res);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
 
 }
